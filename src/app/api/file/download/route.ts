8b73bdd0af1e5ccbcdc172046ce4e1a959613c51
Saeed-Abedini <Saeed78Abedini@gmail.com>
2025-02-09 23:02:07 +0330

    Add file actions api

---
+++ src/app/api/file/download/route.ts
@@ -0,0 +1,54 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { cookies } from "next/headers";
+import { tokenDecoder } from "@/utils";
+
+const s3Client = new S3Client({
+  region: "default",
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY!,
+    secretAccessKey: process.env.S3_SECRET_KEY!,
+  },
+  endpoint: process.env.S3_ENDPOINT,
+  forcePathStyle: true,
+});
+
+export const GET = async (req: Request) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const fileName = searchParams.get("fileName");
+
+    if (!fileName) {
+      return Response.json(
+        { message: "File name is required" },
+        { status: 400 }
+      );
+    }
+
+    const cookieToken = (await cookies()).get("token")?.value;
+    if (!cookieToken)
+      return Response.json({ message: "Unauthorized" }, { status: 401 });
+
+    const verifiedToken = tokenDecoder(cookieToken);
+    if (!verifiedToken)
+      return Response.json({ message: "Invalid token" }, { status: 403 });
+
+    const command = new GetObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: `${fileName}`,
+    });
+
+    const { Body } = await s3Client.send(command);
+    if (!Body)
+      return Response.json({ message: "File not found" }, { status: 404 });
+
+    return new Response(Body as ReadableStream, {
+      headers: {
+        "Content-Type": "audio/ogg",
+        "Content-Disposition": `attachment; filename="${fileName}"`,
+      },
+    });
+  } catch (error) {
+    console.error("Error downloading file:", error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+};
---
+++ src/app/api/file/upload/route.ts
@@ -0,0 +1,52 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { cookies } from "next/headers";
+import { tokenDecoder } from "@/utils";
+
+const s3Client = new S3Client({
+  region: "default",
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY!,
+    secretAccessKey: process.env.S3_SECRET_KEY!,
+  },
+  endpoint: process.env.S3_ENDPOINT,
+  forcePathStyle: true,
+});
+
+export const POST = async (req: Request) => {
+  try {
+    const cookieToken = (await cookies()).get("token")?.value;
+    if (!cookieToken) {
+      return Response.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const verifiedToken = tokenDecoder(cookieToken);
+    if (!verifiedToken) {
+      return Response.json({ message: "Invalid token" }, { status: 403 });
+    }
+
+    const { fileName, fileType } = await req.json();
+
+    if (!fileName || !fileType) {
+      return Response.json(
+        { error: "Invalid file name or type" },
+        { status: 400 }
+      );
+    }
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: fileName,
+      ContentType: fileType,
+    });
+
+    const signedUrl = await getSignedUrl(s3Client, command, {
+      expiresIn: 60 * 5,
+    });
+
+    return Response.json({ url: signedUrl }, { status: 200 });
+  } catch (error) {
+    console.error("Error generating upload URL:", error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+};
