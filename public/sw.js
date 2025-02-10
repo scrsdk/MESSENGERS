092e021669f7bcf73911d9e9dea1e14405715477
Saeed-Abedini <Saeed78Abedini@gmail.com>
2025-02-10 08:24:34 +0330

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("push", (event) => {
+  const data = event.data?.json();
+  const options = {
+    body: data.body,
+    icon: "/icons/icon-192x192.png",
+    badge: "/icons/badge-72x72.png",
+    data: { url: data.url },
+  };
+
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
