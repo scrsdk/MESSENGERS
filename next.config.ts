6dd6c7b3f71aa9c6ab4c294abd603d581da485a1
Saeed-Abedini <Saeed78Abedini@gmail.com>
2025-02-13 23:45:26 +0330

    add react-lottie

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  output: "standalone",
+  experimental: {
+    optimizeCss: true,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -12,6 +16,19 @@ const nextConfig: NextConfig = {
         pathname: "/tlgrm/**",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
