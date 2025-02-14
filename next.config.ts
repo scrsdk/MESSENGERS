df500ebaa3ec5ab832aec2e2d7f384cf1b42e325
Saeed-Abedini <Saeed78Abedini@gmail.com>
2025-02-14 10:45:28 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   output: "standalone",
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -16,19 +12,6 @@ const nextConfig: NextConfig = {
         pathname: "/tlgrm/**",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
   },
 };
 
